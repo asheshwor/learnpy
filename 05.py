6360d84195fb0b84b9fa983f192992b094ee1fd4
asheshwor <asheshwor@gmail.com>
2020-10-02 16:17:49 +0930

    :mask: python practice code

--- 05.py
+++ 05.py
@@ -35,3 +35,77 @@ print(countries)
 print(len(countries))
 countries.append("Norway")
 print(len(countries))
+
+#building a list from another list
+powerball_numbers = [4,8,15,16,23,42]
+def squares(numbers):
+  squares = [] #blank list to start off with
+  for number in numbers:
+    squares.append(number ** 2)
+  return(squares)
+print(squares(powerball_numbers))
+
+def convert_to_floats(numbers):
+  floats = []
+  for number in numbers:
+    floats.append(float(number))
+  return(floats)
+print(convert_to_floats(powerball_numbers))
+print(convert_to_floats([12, 25, 252]))
+
+#even or odd
+def even_or_odd(numbers):
+  results = []
+  for value in numbers:
+    if value % 2 == 0:
+      results.append(True)
+    else:
+      results.append(False)
+  return(results)
+  
+print(even_or_odd(powerball_numbers))
+
+# **note** this is not the best way to this in Python
+
+def only_evens(numbers):
+  evens = []
+  
+  for number in numbers:
+    if number % 2 == 0:
+      evens.append(number)
+  return evens
+  
+print(only_evens([12,45,56,78,77]))
+
+def long_strings(strings):
+  results = []
+  
+  for string in strings:
+    if len(string) >= 5:
+      results.append(string)
+  return results
+  
+print(long_strings(["apple", "lime","watermelon", "dragon fruit", "kiwi", "pea"]))
+
+#append a list using extend method
+mountains = ["Sagarmatha", "K2", "Macchhepucchere", "Dhaulagari", "Kanchenjunga", "Lhotse", "Makalu"]
+print(mountains)   
+extra_mountains = ["Dhaulagari", "Cho Oyu"]
+mountains.extend(extra_mountains)
+print(mountains)
+mountains.extend([])
+print(mountains)
+mountains = ["Sagarmatha", "K2", "Macchhepucchere", "Dhaulagari", "Kanchenjunga", "Lhotse", "Makalu"]
+extra_mountains = ["Dhaulagari", "Cho Oyu"]
+new_mountains =  mountains + extra_mountains
+print(new_mountains)
+ 
+plays = ["Hamlet", "Macbeth", "King Lear"]
+print(plays)
+#insert, NOT override
+plays.insert(1, "Julius Ceasar")
+print(plays)
+plays.insert(0, "Romeo & Juliet")
+print(plays)
+
+
